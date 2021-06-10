ad3bead8ea2019047d75d33319c1c7da3595c027
Pipe Echeverri <pipe.echeverri.1@gmail.com>
2021-06-10 16:21:22 -0500

    CAMBIO DE CONFIGURACION

---
+++ views/admin/rule/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this  yii\web\View */
+/* @var $model mdm\admin\models\BizRule */
+/* @var $form ActiveForm */
+?>
+
+<div class="auth-item-form box box-primary">
+    <div class="box-header with-border"></div>
+    <div class="box-body table-responsive">
+        <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'name')->textInput(['maxlength' => 64]) ?>
+
+        <?= $form->field($model, 'className')->textInput() ?>
+
+        <div class="form-group">
+            <?php
+            echo Html::submitButton($model->isNewRecord ? Yii::t('rbac-admin', 'Create') : Yii::t('rbac-admin', 'Update'), [
+                'class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary'])
+            ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+    </div>
+</div>
