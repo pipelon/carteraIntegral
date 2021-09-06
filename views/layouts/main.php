641bdb426fd2a2b10050e6aa4fb42d991ec2f431
Pipe Echeverri <pipe.echeverri.1@gmail.com>
2021-09-06 16:40:00 -0500

    favicon.ico

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -33,6 +33,7 @@ if (Yii::$app->controller->action->id === 'login' || Yii::$app->controller->acti
         <head>
             <meta charset="<?= Yii::$app->charset ?>"/>
             <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="shortcut icon" href="<?php echo Yii::$app->request->baseUrl; ?>/favicon.ico" type="image/x-icon" />
             <?= Html::csrfMetaTags() ?>
             <title><?= Html::encode($this->title) ?></title>
             <?php $this->head() ?>
