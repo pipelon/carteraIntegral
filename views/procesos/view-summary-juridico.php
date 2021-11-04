b7062577cbcc4ab8a80717993c3ebadbb43766f7
Pipe Echeverri <pipe.echeverri.1@gmail.com>
2021-11-04 17:14:41 -0500

    AJUSTES DE DISEÑO

--- views/procesos/view-summary-juridico.php
+++ views/procesos/view-summary-juridico.php
@@ -20,6 +20,4 @@
     <?php endif; ?>
 </div>
 <div class="modal-footer">
-    <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Close</button>
-    <button type="button" class="btn btn-outline">Save changes</button>
 </div>
--- views/procesos/view-summary-prejuridico.php
+++ views/procesos/view-summary-prejuridico.php
@@ -87,6 +87,5 @@
     <?php endif; ?>
 </div>
 <div class="modal-footer">
-    <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Close</button>
-    <button type="button" class="btn btn-outline">Save changes</button>
+    
 </div>
