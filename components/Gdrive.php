09db09d37a6c3d7b51fb9267845257f0b031657d
DiegoCastaño <diego.castano@talent.com>
2022-06-27 22:39:17 -0500

    ajustes al gdrive para que no lance la excepcion

--- components/Gdrive.php
+++ components/Gdrive.php
@@ -33,7 +33,7 @@ class Gdrive extends Component {
      * @param string $folderId
      * @return list of files and directories
      */
-    public function leerArchivosCarpeta($folderId) {
+    public function leerArchivosCarpetaOLD($folderId) {
         $resultado = $this->service->files->listFiles(array("q" => "'{$folderId}' in parents"));
         $this->out .= "<div class='drive_files'>";
         foreach ($resultado as $elemento) {
@@ -70,6 +70,66 @@ class Gdrive extends Component {
             }
         }
         $this->out .= "</div>";
+        return $this->out;
+    }
+
+    /**
+     * Funcion para leer archivos de una carpeta en google drive
+     * 
+     * @author Diego Castaño <dcastanom@gmail.com>
+     * @copyright 2019 ONICSOFT S.A.S.
+     * @link 
+     * @param string $folderId
+     * @return list of files and directories
+     */
+    public function leerArchivosCarpeta($folderId) {
+        
+        try {
+            $resultado = $this->service->files->listFiles(array("q" => "'{$folderId}' in parents"));
+            if ($resultado->files){
+                $this->out .= "<div class='drive_files'>";
+                foreach ($resultado as $elemento) {
+                    if ($elemento->mimeType == "application/vnd.google-apps.folder") {
+                        $this->out .= "<div class='carpeta'>";
+                        $this->out .= '<div class="name">' . \yii\helpers\Html::img("@web/images/folder_icon.png") . '<a href="https://drive.google.com/open?id=' . $elemento->id . '" target="_blank">' . $elemento->name . '</a></div>';
+                        $this->out .= "</div>";
+                        $this->leerArchivosCarpeta($elemento->id);
+                    } else {
+
+                        $ext = explode(".", $elemento->name);
+                        $ext = end($ext);
+                        switch ($ext) {
+                            case "pdf":
+                                $icon = "pdf_icon.png";
+                                break;
+                            case "xlsx":
+                            case "xlsm":
+                            case "xlsb":
+                            case "xls":
+                                $icon = "excel_icon.png";
+                                break;
+                            case "docx":
+                            case "doc":
+                                $icon = "word_icon.png";
+                                break;
+                            default:
+                                $icon = "plain_icon.png";
+                                break;
+                        }
+                        $this->out .= "<div class='archivo'>";
+                        $this->out .= '<div class="name">' . \yii\helpers\Html::img("@web/images/$icon") . '<a href="https://drive.google.com/open?id=' . $elemento->id . '" target="_blank">' . $elemento->name . '</a></div>';
+                        $this->out .= "</div>";
+                    }
+                }
+                $this->out .= "</div>";
+            }else{
+                $this->out .=  "<span class='not-set'>No se pudo cargar los archivos del drive. Revise el ID de la carpeta.</span>";
+            }
+        } catch (Exception $e) {
+            $this->out .=  "<span class='not-set'>No se pudo cargar los archivos del drive: ".  $e->getMessage(). "</span>";
+        }
+        
+
         return $this->out;
     }
 }
