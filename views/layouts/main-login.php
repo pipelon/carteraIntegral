658113a2bc155cfa0e655660bb617a81c50ccd98
Pipe Echeverri <pipe.echeverri.1@gmail.com>
2022-11-04 15:46:03 -0500

    CAMBIO DE LOGIN

--- views/layouts/main-login.php
+++ views/layouts/main-login.php
@@ -1,4 +1,5 @@
 <?php
+
 use backend\assets\AppAsset;
 use yii\helpers\Html;
 
@@ -10,20 +11,58 @@ dmstr\web\AdminLteAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body class="login-page">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+        <style type="text/css">
+            .login-page{
+                background: #FFFFFF
+            }
+            .bg-login{
+                background: #26365C; 
+                height: 100vh; 
+                background-image: url('../../web/images/bg-login.jpg');
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-size: cover;
+                background-repeat: no-repeat;
+            }
+            .bg-login h1, .bg-login p {
+                color: #FFFFFF;
+                width: 100%;
+                text-align: center;
+            }
+            .login-section{
+                height: 100vh;
+            }
+            .login-section input#loginform-username,
+            .login-section input#loginform-password {
+                border-left: 5px solid #26365C;
+                background: #F9FAFC;
+            }
+            .login-box-msg{
+                color: #26365C;
+                font-weight: bold;
+                font-size: 18px;
+            }
+            @media(max-width:767px) {
+                .bg-login{
+                    display: none;
+                }
+            }
+        </style>
+    </head>
+    <body class="login-page">
 
-<?php $this->beginBody() ?>
+        <?php $this->beginBody() ?>
 
-    <?= $content ?>
+        <?= $content ?>
 
-<?php $this->endBody() ?>
-</body>
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
--- views/site/login.php
+++ views/site/login.php
@@ -20,43 +20,54 @@ $fieldOptions2 = [
 ];
 ?>
 
-<div class="login-box">
-    <div class="login-logo">
-        <?= \yii\helpers\Html::img('@web/images/ciles.png', ['alt' => 'User Image','style' => 'width: 250px']); ?>
-    </div>
-    <!-- /.login-logo -->
-    <div class="login-box-body">
-        <p class="login-box-msg">Ingrese sus claves de acceso</p>
-
-        <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
-
-        <?=
-                $form
-                ->field($model, 'username', $fieldOptions1)
-                ->label(false)
-                ->textInput(['placeholder' => $model->getAttributeLabel('username')])
-        ?>
-
-        <?=
-                $form
-                ->field($model, 'password', $fieldOptions2)
-                ->label(false)
-                ->passwordInput(['placeholder' => $model->getAttributeLabel('password')])
-        ?>
-
-        <div class="row">
-            <div class="col-xs-8">
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-            </div>
-            <!-- /.col -->
-            <div class="col-xs-4">
-                <?= Html::submitButton('Acceder', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
-            </div>
-            <!-- /.col -->
+<div class="row">
+    <div class="col-md-6 bg-login">
+        <div>
+            <h1>BIENVENIDO</h1>
+            <p>Contamos con una plataforma en la cual puedes <br /> consultar el estado de tu proceso en tiempo real.</p>
         </div>
 
-        <?php ActiveForm::end(); ?>
-
     </div>
-    <!-- /.login-box-body -->
-</div><!-- /.login-box -->
+    <div class="col-md-6 login-section">
+        <div class="login-box">
+            <div class="login-logo">
+                <?= \yii\helpers\Html::img('@web/images/ciles.png', ['alt' => 'User Image', 'style' => 'width: 250px']); ?>
+            </div>
+            <!-- /.login-logo -->
+            <div class="login-box-body">
+                <p class="login-box-msg">Ingrese sus claves de acceso</p>
+
+                <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
+
+                <?=
+                        $form
+                        ->field($model, 'username', $fieldOptions1)
+                        ->label(false)
+                        ->textInput(['placeholder' => $model->getAttributeLabel('username')])
+                ?>
+
+                <?=
+                        $form
+                        ->field($model, 'password', $fieldOptions2)
+                        ->label(false)
+                        ->passwordInput(['placeholder' => $model->getAttributeLabel('password')])
+                ?>
+
+                <div class="row">
+                    <div class="col-xs-12">
+                        <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-xs-12">
+                        <?= Html::submitButton('Acceder', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button','style' => 'background: #26365C; border-color: #26365C;']) ?>
+                    </div>
+                    <!-- /.col -->
+                </div>
+
+                <?php ActiveForm::end(); ?>
+
+            </div>
+            <!-- /.login-box-body -->
+        </div><!-- /.login-box -->
+    </div>
+</div>
